90a6da1108e835979d84baec3b0fd060709994e6
Oswaldo Yanez <oswaldo.business31@gmail.com>
2022-10-29 17:02:21 -0400

    agregando mejoras a mobile

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 const navEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const mobileMenu = document.querySelector(".mobile-menu")
+const buguerMenu = document.querySelector(".menu")
 
 navEmail.addEventListener("click", toggleMenu);
+buguerMenu.addEventListener("click", toggleBuguerMenu)
+
 
 function toggleMenu() {
   desktopMenu.classList.toggle("inactive");
+}
+
+function toggleBuguerMenu() {
+    mobileMenu.classList.toggle("inactive")
 }
